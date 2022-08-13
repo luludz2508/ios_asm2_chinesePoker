bd02180b7456e629c34564c0daf753c6b6e75ee0
Luan <s3757937@rmit.edu.vn>
2022-08-13 15:45:54 +0700

    Initial Commit

---
+++ Asm2-ios-rmit/Asm2_ios_rmitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Asm2_ios_rmitApp.swift
+//  Asm2-ios-rmit
+//
+//  Created by Luu Mai Huong on 13/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Asm2_ios_rmitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Asm2-ios-rmit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Asm2-ios-rmit
+//
+//  Created by Luu Mai Huong on 13/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
